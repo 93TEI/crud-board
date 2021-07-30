049fe0d3db60046d4e82421ee2c9ee36fd7e8fa3
93TEI <93louis@gmail.com>
2021-07-30 23:47:23 +0900

    자바 공부 더 하고 와서 여기까지 다시 복습

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -1,15 +1,22 @@
 package com.jojoldu.book.springboot.web;    //web 패키지 : 앞으로의 컨트롤러와 관련된 클래스들은 모두 여기에 담음
 
+import com.jojoldu.book.springboot.web.Dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 //간단한 API를 만든다
-@RestController // 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어준다
+@RestController // 컨트롤러를 'JSON을 반환하는 컨트롤러'로 만들어준다
 public class HelloController {
 
     @GetMapping("/hello")// HTTP Method인 Get의 요청을 받을 수 있는 API를 만들어준다
-    public String hello(){
+    public String hello() {
         return "hello";
     }
 
+    @GetMapping("/hello/dto")
+    //RequestParam은 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
 }
