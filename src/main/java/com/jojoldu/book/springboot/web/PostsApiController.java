85f549609bbd7e891ef661db26d2b9e32553afcd
93TEI <93louis@gmail.com>
2021-08-11 02:19:35 +0900

    수정 머스테치 파일 작성

--- src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -18,12 +18,12 @@ public class PostsApiController {
         return postsService.save(requestDto);
     }
 
-    @PutMapping("/api/v1/posts/{id}") //수정,조회기능
+    @PutMapping("/api/v1/posts/{id}") //수정기능
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
         return postsService.update(id,requestDto);
     }
 
-    @GetMapping("/api/v1/posts/{id}") // 수정,조회기능
+    @GetMapping("/api/v1/posts/{id}") // 조회기능
     public PostsResponseDto findById (@PathVariable Long id)
     {
         return postsService.findById(id);
