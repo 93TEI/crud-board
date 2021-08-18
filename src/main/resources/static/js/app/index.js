9f75f76346a5b3c132767934bdaadfe0d41af43b
93TEI <93louis@gmail.com>
2021-08-19 02:07:41 +0900

    오류 해결 ( 오타 )

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -35,10 +35,11 @@ var main = {
     },
 
     update : function () {
-        var date = {
+        var data = {
             title: $('#title').val(),
             content: $('#content').val()
         };
+
         var id = $('#id').val();
 
         $.ajax({
@@ -62,13 +63,13 @@ var main = {
             type: 'DELETE',
             url: '/api/v1/posts/'+id,
             dataType: 'json',
-            contentType: 'application/json; charset=utf-8'
+            contentType:'application/json; charset=utf-8'
         }).done(function(){
             alert('글이 삭제되었습니다.');
             window.location.href='/';
-        }).fail(function(error)){
+        }).fail(function (error){
             alert(JSON.stringify(error));
-        }
+        });
     }
 
 };
