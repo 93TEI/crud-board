62488d9acdf54eaee3e4c1d15f7eacd46dc6d7a9
93TEI <93louis@gmail.com>
2021-08-29 15:43:09 +0900

    단위테스트에 OAuth 적용

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,9 +2,8 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing  // JPA Auditing 활성화
+// @EnableJpaAuditing  // JPA Auditing 활성화하는것. 근데 나중에 이걸 config에 JpaConfig을 생성하여 옮김
 @SpringBootApplication
 public class Application {
     public static void main(String args[]) {
