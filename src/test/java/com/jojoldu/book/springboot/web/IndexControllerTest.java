f4996bd76ad6f8969aaccb070faf690ceb78aaa4
93TEI <93louis@gmail.com>
2021-09-06 15:35:43 +0900

    한글오류인지 확인(./gradlew test 오류해결중)

--- src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
@@ -23,6 +23,6 @@ public class IndexControllerTest {
         String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(body).contains("teistro");
+        assertThat(body).contains("t");
     }
 }
