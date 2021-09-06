4a3f9daa722778703d40f78c8c23f79115f2ff63
93TEI <93louis@gmail.com>
2021-09-06 14:47:53 +0900

    문자열 비어있는지 확인(./gradlew test 오류해결중)

--- src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
@@ -23,6 +23,6 @@ public class IndexControllerTest {
         String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(body).isNotEmpty();
+        assertThat(body).isNull();
     }
 }
