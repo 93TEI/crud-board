d0f09ca4b3a09723788a071f92c5a4d83b3ec58e
93TEI <93louis@gmail.com>
2021-09-06 16:06:07 +0900

    (./gradlew test 오류해결중)

--- src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
@@ -15,14 +15,13 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 public class IndexControllerTest {
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    TestRestTemplate restTemplate;
 
     @Test
     public void 메인페이지_로딩(){
         //when
         String body = this.restTemplate.getForObject("/", String.class);
 
-        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXX" + body);
         //then
         assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
     }
