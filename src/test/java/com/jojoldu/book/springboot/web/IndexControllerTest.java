46e5cfdbcbe6f0b46893481c55a9335bd6c29f11
93TEI <93louis@gmail.com>
2021-09-06 15:45:33 +0900

    한글오류인지 확인(./gradlew test 오류해결중)

--- src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
@@ -24,7 +24,7 @@ public class IndexControllerTest {
 
         //then
         //assertThat(body).contains("testingtei");
-        Assertions.assertThat(body).isNotEmpty();
-        Assertions.assertThat(body).isNotNull();
+        Assertions.assertThat(body).isEmpty();
+        Assertions.assertThat(body).isNull();
     }
 }
