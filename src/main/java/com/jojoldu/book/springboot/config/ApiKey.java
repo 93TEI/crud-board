36ab2bcda42e93bb5747ed4f26c9a0131bef5150
93TEI <93louis@gmail.com>
2021-10-08 17:34:57 +0900

    fixed

---
+++ src/main/java/com/jojoldu/book/springboot/config/ApiKey.java
@@ -0,0 +1,5 @@
+package com.jojoldu.book.springboot.config;
+
+public class ApiKey {
+    public static String API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiMTYxMTI0ODg1NCIsImF1dGhfaWQiOiIyIiwidG9rZW5fdHlwZSI6IkFjY2Vzc1Rva2VuIiwic2VydmljZV9pZCI6IjQzMDAxMTQ4MSIsIlgtQXBwLVJhdGUtTGltaXQiOiI1MDA6MTAiLCJuYmYiOjE2MzM2Nzk1NzUsImV4cCI6MTY0OTIzMTU3NSwiaWF0IjoxNjMzNjc5NTc1fQ.pgpYSbQrK8yR8z0AUYQBGw6SU2tyZ0f1t17kwSkkz_k";
+}
