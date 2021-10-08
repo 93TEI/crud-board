01e587bb42de37679093303edfce418800c34905
93TEI <93louis@gmail.com>
2021-10-08 17:58:51 +0900

    api관련 설정

--- src/main/java/com/jojoldu/book/springboot/config/ApiKey.java
+++ src/main/java/com/jojoldu/book/springboot/config/ApiKey.java
@@ -1,5 +1,24 @@
 package com.jojoldu.book.springboot.config;
 
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
 public class ApiKey {
-    public static String API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiMTYxMTI0ODg1NCIsImF1dGhfaWQiOiIyIiwidG9rZW5fdHlwZSI6IkFjY2Vzc1Rva2VuIiwic2VydmljZV9pZCI6IjQzMDAxMTQ4MSIsIlgtQXBwLVJhdGUtTGltaXQiOiI1MDA6MTAiLCJuYmYiOjE2MzM2Nzk1NzUsImV4cCI6MTY0OTIzMTU3NSwiaWF0IjoxNjMzNjc5NTc1fQ.pgpYSbQrK8yR8z0AUYQBGw6SU2tyZ0f1t17kwSkkz_k";
+    @Value("${apikey}")
+    private String key;
+
+    private static ApiKey instance = new ApiKey();
+
+    private ApiKey() {
+    }
+
+    public static ApiKey getInstance() {
+        if (instance == null) {
+            instance = new ApiKey();
+        }
+        return instance;
+    }
 }
