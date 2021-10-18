bf34be088e394f7a7e4f524ce2e21e717ca78718
93TEI <93louis@gmail.com>
2021-10-18 21:20:40 +0900

    주석 업데이트

--- src/main/java/com/jojoldu/book/springboot/web/Dto/PostsSaveRequestDto.java
+++ src/main/java/com/jojoldu/book/springboot/web/Dto/PostsSaveRequestDto.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class PostsSaveRequestDto {
+public class PostsSaveRequestDto { // 뷰 레이어. 뷰를 위한 클래스로 Posts 엔티티 클래스와 역할을 철저히 분리해야함. 뷰를 위한 클래스는 자주 변경되기 때문
     private String title;
     private String content;
     private String author;
